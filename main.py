753fc1fdee968e3be5114e3de5d4d44da53cbc10
mcosta <mateusjrcosta@hotmail.com>
2025-03-09 23:18:15 -0300

    salvando exercicios

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import json
 from utils.prompt_builder import *
 from services.OpenAIClient.openai_client import * 
+from services.DBClient.db_client import insert_data
 
 def lambda_handler(event, context):
     try:
         body = json.loads(event.get("body", "{}"))
         prompt = createprompt(body=body)
         training_plan = call_openai(prompt)
-        
-        with open(r"database\output\workoutplan.json", "w", encoding="utf-8") as file:
-            file.write(training_plan)
-
+        training_plan = json.loads(training_plan.replace("`", "").replace("json", ""))
+        training_plan['user_id'] = json.loads(event['body'])['new_member']['user_data']['user_id']
+        insert_data(data=training_plan, table_name='workout_plans')
     except Exception as e:
         return {
             "statusCode": 500,
@@ -19,40 +19,3 @@ def lambda_handler(event, context):
                 "Content-Type": "application/json"
             }
         }
-
-if __name__=='__main__':
-    event = {
-        "body": json.dumps({  # Converte o dicionário para string JSON
-        "new_member": {
-            "personal_data": {
-                "gender":"male",
-                "weight": "50",
-                "height": "162"
-            },
-            "user_purpose": {
-                "main_objective": "Gain Muscle",
-                "secondary_objective": "Tone and define"
-            },
-            "training_preferences": {
-                "experience_level": "intermediate",
-                "training_frequency_per_week": 5
-            },
-            "health_conditions": {
-                "pre_diabetes": False,
-                "high_cholesterol": False,
-                "spinal_disorder": False,
-                "hypertension": False,
-                "dizziness_or_fainting": False,
-                "other_health_issues": "Tenho desgaste na lombar e escoliose"
-            },
-            "affected_areas": {
-                "knee": False,
-                "shoulder": False,
-                "elbow": False,
-                "spine": False,
-                "lower_back": False
-            }
-        }
-    })
-    }
-    lambda_handler(event=event, context=None)
--- services/DBClient/db_client.py
+++ services/DBClient/db_client.py
@@ -1,6 +1,7 @@
 import psycopg2
 from services.AWSClient.aws_services import parameter_store
 import json 
+import sys 
 
 def dbconnect():
     print('[INFO] Desempacotando credenciais.')
@@ -16,8 +17,45 @@ def dbconnect():
             password=credentials['pwd']
         )
         return connection
-    except Exception as error:
+    except psycopg2.OperationalError as error:
         print(f'[ERROR] Nao foi possivel estabelecer conexao, erro: {error}')
 
-def insert_data():
-    pass
+def insert_data(data, table_name):
+    conn = dbconnect()
+    if conn is None:
+        print("[ERROR] Falha na conexão com o banco de dados.")
+        sys.exit(0)
+
+    cursor = conn.cursor()
+
+    try:
+        for day_trainning in data["training_plan"]:
+            day = day_trainning['day']
+            exercises = day_trainning['exercises'] 
+            user_id = data['user_id']
+
+            for exercise in exercises:
+                muscle_groups = exercise['muscle_groups']
+                name = exercise['name']
+                sets = exercise['sets']
+                reps = exercise['reps']
+                tips = exercise['tips']
+                alternatives = exercise['alternatives']
+                affected_muscles = exercise['affected_muscles']
+
+                query = f"""
+                INSERT INTO workout_plans (user_id, day, muscle_group, name, sets, reps, tips, alternatives, affected_muscles)
+                VALUES ('{user_id}', '{day}', '{muscle_groups}', '{name}', '{sets}', 
+                '{reps}', '{tips}', '{json.dumps(alternatives)}', '{json.dumps(affected_muscles)}');
+                """
+
+                cursor.execute(query)
+                conn.commit()
+    
+        print("[INFO] Dados inseridos com sucesso!")
+    except Exception as error:
+        print(f"[ERROR] Erro ao inserir dados: {error}")
+        conn.rollback()
+    finally:
+        cursor.close()
+        conn.close()
